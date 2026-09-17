1952e87d323203e7dbf050126783c52cf8664b03
Mya-Codeszz <mya.humphrey@mitglobalonline.org>
2026-09-17 16:25:19 -0700

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,6 @@
 import { createBrowserClient } from '@supabase/ssr';
 import type { Database } from '@/types/database.types';
 
-// Use this client inside Client Components ("use client").
 export function createClient() {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
