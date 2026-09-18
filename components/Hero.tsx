e2001ed4b3e8592ef46089b81ab2a9d3791f461c
Mya-Codeszz <mya.humphrey@mitglobalonline.org>
2026-09-18 12:54:53 -0700

    Add files via upload

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -280,6 +280,21 @@ export default function Hero() {
               View My Schedule <ArrowRight className="h-3.5 w-3.5" />
             </Link>
           </div>
+
+          <div className="absolute right-[-8px] top-[250px] z-30 hidden w-36 rotate-[7deg] font-script text-2xl leading-6 text-[#174534] lg:block">
+            <span className="block">small</span>
+            <span className="block pl-6">steps</span>
+            <span className="block pl-3">big change</span>
+            <span className="mt-1 block text-right text-3xl">♡</span>
+          </div>
+
+          <div className="absolute bottom-10 right-[-4px] z-30 hidden -rotate-[5deg] font-script text-xl leading-6 text-[#174534] xl:block">
+            find what
+            <br />
+            moves you
+            <br />
+            <span className="text-2xl">♡</span>
+          </div>
         </div>
       </div>
     </section>
